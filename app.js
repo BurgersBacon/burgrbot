98779afb74d4f51004b57dc3a1278ae8fb026f22
Jeison Stive Ruiz Carrillo <jruiz@novoscale.com>
2018-06-20 16:32:45 -0500

    testing new structure and trying new commands

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-const { Client } = require('discord.js');
-const bot = new Client();
-const cfg = require('./config.json');
+// Discord dependencies
+const Discord = require('discord.js');
+const newUsers = new Discord.Collection();
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+// Other dependencies
 const fs = require('fs');
+
+// Config files
+const { prefix, token } = require('./config.json');
+
+// Firebase files
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey.json');
 const firebaseDb = require('./firebase-db.json');
 
+// Commands thing
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: firebaseDb.databaseURL
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: firebaseDb.databaseURL,
 });
 
 // var database = firebase.database();
-var db = admin.firestore();
-
-var users = db.collection('users');
+// var db = admin.firestore();
+//
+// var users = db.collection('users');
 //
 // dinosaruios.doc('terodac').set(
 //   {
@@ -38,75 +55,96 @@ var users = db.collection('users');
 //     .catch(err => {
 //       console.log('Error getting document', err);
 //     });
+//
+// var dinosauriosQueSeComenOtrosDinos = users.where('name', '==', 'jason').get().then(function(querySnapshot) {
+//         querySnapshot.forEach(function(doc) {
+//             // doc.data() is never undefined for query doc snapshots
+//             console.log(doc.id, ' => ', doc.data());
+//         });
+//     })
+//     .catch(function(error) {
+//         console.log('Error getting documents: ', error);
+//     });
 
-var dinosauriosQueSeComenOtrosDinos = users.where('name', '==', 'jason').get().then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-        });
-    })
-    .catch(function(error) {
-        console.log("Error getting documents: ", error);
-    });
+client.on('ready', () => {
+	console.log('-----------------------------------------------------');
+	client.user.setActivity(`${prefix} help`);
 
-bot.on('ready', () => {
-  console.log("-----------------------------------------------------");
-  bot.user.setActivity(`${cfg.prefix} help`);
-
-  console.log("lmaooo", bot.user.bot);
+	console.log('lmaooo', client.user.bot);
 });
 
-bot.on('message', msg => {
-  if (msg.author.bot || !msg.content.startsWith(cfg.prefix)) return;
-  const args = msg.content.slice(cfg.prefix.length + 1).split(' ');
-  const command = args.shift().toLowerCase();
-
-  if (command === 'hi') {
-    msg.reply('que dices, bro');
-  }
-  else if (command === 'photo') {
-    msg.reply(msg.author.avatarURL);
-  }
-  else if (command === 'roll'){
-    let max;
-    args.length>0?max=args[0]:max=100;
-    let randomNumber = Math.floor((Math.random() * max));
-    let description = msg.member.nickname + " lanza los dados y obtiene un... `" + randomNumber + "`";
-
-    msg.channel.send({
-      "embed": {
-        "title": "🎲 Número Aleatorio",
-        "description": description,
-        "color": 16098851,
-        "footer": {
-          "icon_url": "https://burgersbacon.github.io/portfolio/buurgrbit.png",
-          "text": `en una escala de 1 - ${max}`
-        },
-        "thumbnail": {
-          "url": "https://burgersbacon.github.io/portfolio/dice.png"
-        }
-      }
-    });
-  }
-  else if (command === 'help') {
-    msg.channel.send({
-      "embed": {
-        "title": "Lista de comandos",
-        "description": "`help`, `hi`, `roll`, `drogas`, `poll`",
-        "color": 16098851,
-        "footer": {
-          "icon_url": "https://burgersbacon.github.io/portfolio/buurgrbit.png",
-          "text": `recuerda usar "${cfg.prefix}" antes de cada comando.`
-        },
-        "thumbnail": {
-          "url": "https://burgersbacon.github.io/portfolio/assets/images/contact.png"
-        }
-      }
-    });
-  }
-  else{
-    msg.channel.send("emhh ese comando no existe xd, para ver la lista de comandos usa '.burgr help'");
-  }
+client.on('guildMemberAdd', (member) => {
+	const guild = member.guild;
+	newUsers.set(member.id, member.user);
+	if (newUsers.size > 10) {
+		const defaultChannel = guild.channels.find(c=> c.permissionsFor(guild.me).has('SEND_MESSAGES'));
+		const userlist = newUsers.map(u => u.toString()).join(' ');
+		defaultChannel.send('Bienvenido' + userlist);
+		newUsers.clear();
+	}
 });
 
-bot.login(cfg.token);
+client.on('guildMemberRemove', (member) => {
+	if(newUsers.has(member.id)) newUsers.delete(member.id);
+});
+
+client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (msg.content.includes('bot')) {
+      msg.react('446395864072585229');
+  }
+  if (msg.content.startsWith(prefix)) {
+    const args = msg.content.slice(prefix.length + 1).split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === 'hi') {
+      msg.reply('que dices, bro');
+    }
+    else if (command === 'photo') {
+      msg.reply(msg.author.avatarURL);
+    }
+    else if (command === 'roll') {
+      client.commands.get('roll').execute(msg, args);
+    }
+    else if (command === 'sup') {
+      msg.channel.send(`Bro, ${msg.author} te envié un mensaje directo.`);
+      msg.author.send('hello');
+    }
+    else if (command === 'lol') {
+      msg.react('446395907823501314');
+    }
+    else if(command === 'listemojis') {
+      const emojiList = msg.guild.emojis.map((e, x) => (x + ' = ' + e) + ' | ' + e.name).join('\n');
+      msg.channel.send(emojiList);
+    }
+		else if (command === 'server') {
+			console.log(msg.guild);
+			msg.channel.send(`Server name: ${msg.guild.name}\nTotal members: ${msg.guild.memberCount}`);
+		}
+    else if (command === 'help') {
+      msg.channel.send({
+        'embed': {
+          'title': 'Lista de comandos',
+          'description': '`help`, `hi`, `roll`, `drogas`, `poll`',
+          'color': 16098851,
+          'footer': {
+            'icon_url': 'https://burgersbacon.github.io/portfolio/buurgrbit.png',
+            'text': `recuerda usar '${prefix}' antes de cada comando.`,
+          },
+          'thumbnail': {
+						'url': 'https://burgersbacon.github.io/portfolio/assets/images/contact.png',
+					},
+				},
+			});
+		}
+		else{
+			msg.channel.send('emhh ese comando no existe xd, para ver la lista de comandos usa ".burgr help"');
+		}
+	}
+	else{
+		return;
+	}
+
+});
+
+client.login(token);
---
+++ commands/roll.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: 'roll',
+    description: 'Rolls a dice',
+    execute(msg, args) {
+      let max;
+      args.length > 0 ? max = args[0] : max = 100;
+      const randomNumber = Math.floor((Math.random() * max));
+      const description = msg.author.nickname + ' lanza los dados y obtiene un... `' + randomNumber + '`';
+
+      msg.channel.send({
+        'embed': {
+          'title': '🎲 Número Aleatorio',
+          'description': description,
+          'color': 16098851,
+          'footer': {
+            'icon_url': 'https://burgersbacon.github.io/portfolio/buurgrbit.png',
+            'text': `en una escala de 1 - ${max}`,
+          },
+          'thumbnail': {
+            'url': 'https://burgersbacon.github.io/portfolio/dice.png',
+          },
+        },
+      });
+    },
+};
